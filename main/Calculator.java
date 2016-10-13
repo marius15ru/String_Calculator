3f20a35cc5a02cb4992af0cb4c07a55fde43fab0
marius15ru <marius15@ru.is>
2016-10-13 20:51:03 +0000

    3. Add With New Line, var bent á að, að nota String.split væri margfalt einfaldara

--- main/Calculator.java
+++ main/Calculator.java
@@ -2,34 +2,18 @@ package stringcalculator;
 
 public class Calculator{
     public static int add(String args){
-		StringBuilder sb = new StringBuilder(args);
-		int sum = 0, index1 = 0, index2 = 0, ifC = sb.indexOf(","), ifNL = sb.indexOf("\n");
+		int sum = 0;
+		String numbers[] = args.split(",|\n");
 
 	 	if(args == ""){
 			return 0;
 		}
 		else if(args != "")
 		{
-    		index2 = ifC < ifNL ? ifC : ifNL;
-		    if(index2 == -1)
-    			index2 = ifC > ifNL ? ifC : ifNL;
+		    for(String n : numbers)
+		      sum += Integer.parseInt(n);
 		    
-		    while(index2 != -1)
-		    {
-		      sum += Integer.parseInt(sb.substring(index1, index2));
-		      index1 = index2+1;
-		      
-		      ifC = sb.indexOf(",", index1); 
-		      ifNL = sb.indexOf("\n", index1);
-
-		      index2 = ifC < ifNL ? ifC : ifNL;
-		      if(index2 == -1)
-    			index2 = ifC > ifNL ? ifC : ifNL;
-		    }
-
-		    index2 = sb.length();
-		    sum += Integer.parseInt(sb.substring(index1, index2));
-			return sum;
+		    return sum;
 		}
 		else
 			return -1;
